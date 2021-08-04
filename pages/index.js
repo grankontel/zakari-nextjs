2426a2dbc1cab48469594175712b50c21ad2b408
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-08-04 22:42:02 +0200

    added copy button

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,11 @@ import {
 import { useState } from "react";
 import useUser from "../lib/useUser";
 import postZakari from "../lib/postZakari";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 const ZakariForm = ({ ...props }) => {
-  const  [isLoading, setIsLoading]  = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [copied, setCopied] = useState(false);
   const { user, mutateUser } = useUser();
 
   const [request, setRequest] = useState("");
@@ -21,60 +23,73 @@ const ZakariForm = ({ ...props }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    setErrorMessage('')
-    setResponse('')
-    setIsLoading(true)
 
-    const resp = postZakari(process.env.NEXT_PUBLIC_ZAKARI_ENDPOINT, user.token, request);
-    resp.then(data => {
-      setIsLoading(false)
+    setErrorMessage("");
+    setResponse("");
+    setIsLoading(true);
+    setCopied(false);
+
+    const resp = postZakari(
+      process.env.NEXT_PUBLIC_ZAKARI_ENDPOINT,
+      user.token,
+      request
+    );
+    resp.then((data) => {
+      setIsLoading(false);
 
       if (data.errors !== undefined) {
-        setErrorMessage('Erreur de zakari')
+        setErrorMessage("Erreur de zakari");
       } else {
-        setResponse(data.response.message)
+        setResponse(data.response.message);
       }
-    })
-    
+    });
   };
 
   return (
     <div {...props}>
-      <form>
-        <BForm.Field>
-          <BForm.Label>Tèks pou korijé</BForm.Label>
-          <BForm.Control>
-            <BForm.Textarea
-              name="source"
-              onChange={(e) => setRequest(e.target.value)}
-              value={request}
-              required
-            />
-          </BForm.Control>
-        </BForm.Field>
-        <Message color="primary">
-          <Message.Header>Répons</Message.Header>
-          <Message.Body>{response}</Message.Body>
-        </Message>
-        {errorMessage.length > 0 && (
-          <Notification className="error" mt={2} light color="danger">
-            <Button remove onClick={eraseErrorMessage} />
-            {errorMessage}
-          </Notification>
-        )}
-        <hr />
-        <Button.Group align="right">
+      <BForm.Field>
+        <BForm.Label>Tèks pou korijé</BForm.Label>
+        <BForm.Control>
+          <BForm.Textarea
+            name="source"
+            onChange={(e) => {
+              setRequest(e.target.value);
+              setCopied(false);
+            }}
+            value={request}
+            required
+          />
+        </BForm.Control>
+      </BForm.Field>
+      <Message color="primary">
+        <Message.Header>Répons</Message.Header>
+        <Message.Body>{response}</Message.Body>
+      </Message>
+      {errorMessage.length > 0 && (
+        <Notification className="error" mt={2} light color="danger">
+          <Button remove onClick={eraseErrorMessage} />
+          {errorMessage}
+        </Notification>
+      )}
+      <hr />
+      <Button.Group align="right">
+        <CopyToClipboard text={response} onCopy={() => setCopied(true)}>
           <Button
-            color="primary"
-            onClick={handleSubmit}
-            disabled={!user?.isLoggedIn || (!isLoading && request.length < 2)}
-            loading={isLoading}
+            color={copied ? "info" : "light"}
+            disabled={!user?.isLoggedIn || response === ""}
           >
-            Korijé
+            {copied ? "I adan !" : "Kopyé"}
           </Button>
-        </Button.Group>
-      </form>
+        </CopyToClipboard>
+        <Button
+          color="primary"
+          onClick={handleSubmit}
+          disabled={!user?.isLoggedIn || (!isLoading && request.length < 2)}
+          loading={isLoading}
+        >
+          Korijé
+        </Button>
+      </Button.Group>
     </div>
   );
 };

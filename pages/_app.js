068ba906d3cfed58c4d8a65fa09b49e4193ad76b
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2021-08-05 18:29:20 +0200

    remove old stuff

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,11 @@
 import { Provider } from "next-auth/client";
-import { SWRConfig } from "swr";
-import fetch from "../lib/fetchJson";
 import "../styles/mystyles.scss";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SWRConfig
-      value={{
-        fetcher: fetch,
-        onError: (err) => {
-          console.error(err);
-        },
-      }}
-    >
-      <Provider>
-        <Component {...pageProps} />
-      </Provider>
-    </SWRConfig>
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
   );
 }
 
